6e56b193fc4fc61d51f2af62ff83ed7b602ffbac
Harutyun Mnatsakanyan <harmn90@gmail.com>
2019-01-11 23:16:23 +0400

    add form

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,17 +1,37 @@
-
-import {Component, OnInit, Inject, PLATFORM_ID} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'homepage',
-    templateUrl: 'homepage.component.html',
-    styleUrls: []
+    templateUrl: './homepage.component.html',
+    styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
 
-    constructor() {
+    public searchForm: FormGroup;
+    private errorMessages: string;
+
+    constructor(
+        private FormBuilder: FormBuilder
+    ) {
     }
 
     ngOnInit() {
+        this.buildForm();
+    }
 
+    private buildForm(): void {
+        this.searchForm = this.FormBuilder.group({
+            'country': new FormControl('', [Validators.required]),
+            'zip_code': new FormControl('', [Validators.required]),
+        });
+    }
+
+    public submit(): void {
+        if (this.searchForm.invalid) {
+            return;
+        }
+        // this.formNotSubmited = false;
+        // this.userService.resetPassword(this.resetForm.value);
     }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,95 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {ErrorPageComponent} from './error-page/error-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+    MatButtonModule, MatIconModule, MatMenuModule, MatSnackBarModule,
+    MatToolbarModule, MatCardModule, MatAutocompleteModule, MatButtonToggleModule, MatCheckboxModule, MatChipsModule,
+    MatTableModule, MatDatepickerModule, MatDialogModule, MatExpansionModule, MatFormFieldModule, MatGridListModule,
+    MatInputModule, MatListModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule,
+    MatRippleModule, MatSelectModule, MatSidenavModule, MatSlideToggleModule, MatSliderModule, MatSortModule,
+    MatTabsModule, MatTooltipModule, MatNativeDateModule
+} from '@angular/material';
 
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        FormsModule,
+        ReactiveFormsModule,
+        FlexLayoutModule,
+        MatIconModule,
+        MatCardModule,
+        MatAutocompleteModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatCheckboxModule,
+        MatChipsModule,
+        MatTableModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatExpansionModule,
+        MatFormFieldModule,
+        MatGridListModule,
+        MatInputModule,
+        MatListModule,
+        MatMenuModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatRippleModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSlideToggleModule,
+        MatSliderModule,
+        MatSnackBarModule,
+        MatSortModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        MatNativeDateModule,
     ],
     declarations: [
         ErrorPageComponent,
     ],
     exports: [
-        ErrorPageComponent
+        ErrorPageComponent,
+        FormsModule,
+        ReactiveFormsModule,
+        FlexLayoutModule,
+        MatIconModule,
+        MatCardModule,
+        MatAutocompleteModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatCheckboxModule,
+        MatChipsModule,
+        MatTableModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatExpansionModule,
+        MatFormFieldModule,
+        MatGridListModule,
+        MatInputModule,
+        MatListModule,
+        MatMenuModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatRippleModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSlideToggleModule,
+        MatSliderModule,
+        MatSnackBarModule,
+        MatSortModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        MatNativeDateModule,
     ]
 })
 export class SharedModule {
